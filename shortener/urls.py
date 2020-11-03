61d51a594c75bb3fc59cb7622d2c3ac5b5164ce0
mahdibohloul <mahdi_bohloul@yahoo.com>
2020-11-03 16:56:07 +0330

    some filed deleted

--- shortener/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='shortener-home'),
-    path('shortener/', views.shortener_urls, name='shortener-urls'),
-    path('shortener/redirections/', views.urls_list, name='redirections-urls'),
-    path('shortener/filter/', views.urls_filter, name='filter-urls'),
-]
